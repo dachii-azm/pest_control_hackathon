2abb3332882d5dc96a73034e903d29c5544fc927
dachii-azm <alohadaichi@gmail.com>
2023-06-21 08:14:14 +0900

    feat: scraping

---
+++ utils/scraping.py
@@ -0,0 +1,27 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import urllib
+import time
+
+WORD='ホシガラス'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options)
+# Create url variable containing the webpage for a Google image search.
+url = ("https://www.google.com/search?q={s}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568")
+# Launch the browser and open the given url in the webdriver.
+driver.get(url.format(s=WORD))
+# Scroll down the body of the web page and load the images.
+driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+time.sleep(5)
+# Find the images.
+imgResults = driver.find_elements(By.XPATH,"//img[contains(@class,'Q4LuWd')]")
+# Access and store the scr list of image url's.
+src = []
+for img in imgResults:
+    src.append(img.get_attribute('src'))
+# Retrieve and download the images.
+for i in range(1000):    urllib.request.urlretrieve(str(src[i]),"sample_data/{}_{}.jpg".format(WORD, i))
